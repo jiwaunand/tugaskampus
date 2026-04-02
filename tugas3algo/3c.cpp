9701f852aef8b6ba395c8da66bb29941c08fbfea
JiwaArch <2511512002_muhammad@unand.ac.id>
2026-04-02 16:42:20 +0700

    update 3c

--- tugas3algo/3c.cpp
+++ tugas3algo/3c.cpp
@@ -3,11 +3,12 @@
 
 int main(){
 	for(int i=1;i<=N;i++) {
-		for(int j=1;j<=i;j++) {
-			printf("1 ");
-		}
-		for(int k=0;k<=N-i;k++) {
-			printf("0 ");
+		for(int j=1;j<=N;j++) {
+			if(j<=i) {
+				printf("1 ");
+			} else {
+				printf("0 ");
+			}
 		}
 		printf("\n");
 	}
